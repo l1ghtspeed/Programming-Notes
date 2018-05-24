38f3019be8fd34429ac166fb94bc3bde854c3a03
Zi <zi.s.gao@gmail.com>
2018-05-24 11:50:54 -0400

    Fixed queue example

--- Data Structures/Java/Queue.java
+++ Data Structures/Java/Queue.java
@@ -3,6 +3,7 @@ public class Queue {
     private int[] queue = new int[4];
     private int head = -1;
     private int tail = 0;
+    private boolean filled = false;
 
     public Queue(){    
     }
@@ -13,25 +14,29 @@ public class Queue {
             this.head = 0;
         } else if (this.head >= this.queue.length){
             increase(false);
-        } else if (this.head == this.tail){
+        } else if (this.head == this.tail && this.filled == true){
             increase(true);
         }
         this.queue[this.head] = value;
+        this.filled = true;
     }
 
     public void dequeue(){
         if (this.head != this.tail){
             this.queue[this.tail] = 0;
             this.tail++;
+            if (this.head == this.tail){
+                this.filled = false;
+            }
             if (this.tail >= this.queue.length){
                 this.tail = 0;
             }    
-        }
+        } 
     }
 
 
     public int peek(){
-        return this.queue[this.head];
+        return this.queue[this.tail];
     }
 
     public boolean isEmpty(){
@@ -57,9 +62,22 @@ public class Queue {
 
     public String toString(){
         String temp = "";
-        for (int i = this.tail; i < this.head; i++){
-            temp += this.queue[i];
-            temp += ", ";
+        System.out.println(this.tail);
+        System.out.println(this.head);
+        if (this.head < this.tail){
+            for (int i = 0; i < this.head+1; i++){
+                temp += this.queue[i];
+                temp += ", ";
+            }
+            for (int i = this.tail; i < this.queue.length; i++){
+                temp += this.queue[i];
+                temp += ", ";
+            }
+        } else {
+            for (int i = this.tail; i < this.head+1; i++){
+                temp += this.queue[i];
+                temp += ", ";
+            }
         }
         return temp;
     }
--- Data Structures/Java/Test.java
+++ Data Structures/Java/Test.java
@@ -5,12 +5,15 @@ public class Test {
         Queue a = new Queue();
         
         
+        a.enqueue(1);
         a.enqueue(2);
         a.enqueue(3);
-
         System.out.println(a);
-        
         a.dequeue();
+        a.dequeue();
+        a.enqueue(4);
+        a.enqueue(5);
+        a.enqueue(6);
 
         System.out.println(a);
         
