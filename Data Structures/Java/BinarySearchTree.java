a42e992bdc008d9560f636187a522ae153932694
Zi <zi.s.gao@gmail.com>
2018-05-29 15:16:50 -0400

    worked on binary search tree java implementation

--- Data Structures/Java/BinarySearchTree.java
+++ Data Structures/Java/BinarySearchTree.java
@@ -1,7 +1,69 @@
 public class BinarySearchTree{
 
-    public BinarySearchTree(){
 
+    public BinarySearchTree(){
+    }
+
+    public void insert(E value){
+
+    }
+
+    public void delete(){
+
+    }
+
+    public E Find(){
+
+    }
+
+    public void display(){
+        
+    }
+
+    private class Node<E> {
+
+        public Node<E> left;
+        public Node<E> right; 
+        public Node<E> parent; 
+        public E value;
+    
+        public Node(Node<E> _left, Node<E> _right, Node<E> _parent, E _value){
+    
+            this.left = _left;
+            this.right = _right;
+            this.parent = _parent;
+            this.value = _value;
+            
+        }
+    
+        public Node(E _value){
+            this.parent = null;
+            this.left = null;
+            this.right = null;
+            this.value = _value;
+        }
+
+        public Node(Node<E> _parent, E _value){
+            this.parent = _parent;
+            this.left = null;
+            this.right = null;
+            this.value = _value;
+        }
+    
+        /**
+         * @param left the next to set
+         */
+        public void setLeft(Node<E> _left) {
+            this.left = _left;
+        }
+    
+        public void setRight(Node<E> _right){
+            this.right = _right;
+        }
+
+        public void setParent(Node<E> _parent){
+            this.parent = _parent;
+        }
     }
 
 
