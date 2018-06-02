6d4238968c661a8c0b1641504a1e688fe50ee6d0
unknown <zi.s.gao@gmail.com>
2018-06-02 15:49:29 -0400

    created and started jsOverview and es6Overview

---
+++ Programming Languages/Javascript/es6Overview.js
@@ -0,0 +1,11 @@
+/* 
+
+ES6, or EMCAScript6, is a (somewhat recent) huge update on the Javascript language. 
+This new standard provides plenty of powerful and useful features, but also depricates some old ones.
+Proficiency in the new ES6 features is a must for any JS developers, whether it be for front end or back end.
+Below are some of the more important ES6 features, along with examples.
+For a full list on all the changes, visit this page: http://es6-features.org/#Constants
+
+*/
+
+
---
+++ Programming Languages/Javascript/jsOverview.js
@@ -0,0 +1,17 @@
+/*
+
+Javascript is a powerful, function forward scripting language. 
+
+Thanks to the numerous frameworks developed to support Javascript, 
+this popular programming language can be used to create a wide variety of things.
+
+There is React, Angular, Vue, jQuery, and many others for front end web developement.
+There is Node, Express, Meteor, Feather and many others for back end web development.
+There is even React Native for native app development.
+
+While it's not perfect, Javascript is still a language that I would recommend to all programmers. 
+This is in no way a complete guide to the Javascript language, but there will be some important JS features and examples below.
+
+*/
+
+
