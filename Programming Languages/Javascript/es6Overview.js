ed8d2359b2654052ce41551809c5b1cd67b1309d
unknown <zi.s.gao@gmail.com>
2018-06-04 07:33:54 -0400

    worked on es6Overview

--- Programming Languages/Javascript/es6Overview.js
+++ Programming Languages/Javascript/es6Overview.js
@@ -9,3 +9,109 @@ For a full list on all the changes, visit this page: http://es6-features.org/#Co
 */
 
 
+/*
+
+Arrow Functions:
+
+General syntax: function_name = (function parameters) => {};
+Is basically a shorthand form for normal function notation, but binds 'this' differently.
+
+*/
+
+let generic_arrow_func = (a, b) => { return a+b };
+
+let simple_arrow_func = a => a + 2;
+
+let num = 4;
+
+console.log(simple_arrow_func(4));
+
+
+/*
+
+Class:
+
+Javascript now has "classes" - hooray! 
+Javascript is still not a lanugage with an object oriented programming model, but now it has syntactic sugar that makes it look like one.
+This topic is a rather lengthy one, so for everything you need to know about "class" in JS, visit https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes  
+
+*/
+
+class Snack {
+
+    constructor(name, brand) {
+        this.name = name;
+        this.brand = brand;
+    }
+
+    static definition(){
+        console.log("Snacks are fun to eat!");
+    }
+
+    eat() {
+        console.log(this.name + ' by ' + this.brand + ' is fun to eat!');
+    }
+
+}
+
+class Chocolate extends Snack {
+
+    constructor(name, brand) {
+        super(name, brand);
+    }
+
+    static definition(){
+        console.log("Chocolates are sweet!");
+    }
+
+    eat() {
+        console.log(this.name + ' by ' + this.brand + ' is awesome chocolate');
+    }
+
+}
+
+let kitkat = new Chocolate('Kitkat', 'Nestle');
+
+kitkat.eat();
+Chocolate.definition();
+
+
+/*
+
+String Interpolation:
+
+Ever struggled with multiline strings? Or including javascript inside strings?
+Say no more.
+
+*/
+
+let some_string = 'And can have embedded JS!'
+
+let this_is_awesome = `This string
+
+    is multi
+
+    lined!
+
+    ${some_string}
+
+    How awesome is that?
+`;
+
+console.log(this_is_awesome);
+
+
+/*
+
+Default:
+
+Default function parameter values. Why didn't JS have this before?
+
+*/
+
+let get_area = (height, width=10) => {return height*width};
+
+
+/*
+
+*/
