1321383c44ea22adc24fe4f53b72bd868ffc18f4
unknown <zi.s.gao@gmail.com>
2018-06-11 07:00:44 -0400

    Edited binary search tree

--- Data Structures/Java/BinarySearchTree.java
+++ Data Structures/Java/BinarySearchTree.java
@@ -1,22 +1,58 @@
 public class BinarySearchTree{
 
-
+    private Node<E> root = null;
+    
     public BinarySearchTree(){
     }
 
     public void insert(E value){
+        if (this.root == null){
+            this.root = new Node<E>(value);
+        } else {
+            this.insert(value, this.root);
+        }
+    }
 
+    public void insert(E value, Node<E> curr){
+        if (value <= curr.value && curr.left == null){
+            curr.left = new Node<E>(value);
+            curr.left.parent = curr;
+        } else if (value <= curr.value){
+            this.insert(value, curr.left);
+        } else if (value > curr.value && curr.right == null){
+            curr.right = new Node<E>(value);
+            curr.right.parent = curr;
+        } else {
+            this.insert(value, curr.right);
+        }
     }
 
     public void delete(){
 
     }
 
-    public E Find(){
+    public Boolean Find(E value){
 
     }
 
-    public void display(){
+    public String display(String notation){
+        String temp = "";
+        Node<E> curr = this.root;
+        if (notation == "in-order"){
+            while (curr.left != null){
+                curr = curr.left;
+            }
+            while (curr.parent != null){
+
+            }
+            return temp;
+        } else if (notation == "pre-order"){
+            return temp;
+        } else if (notation == "post-order"){
+            return temp;
+        } else {
+            return "Invalid notation format";
+        }
         
     }
 
@@ -27,24 +63,8 @@ public class BinarySearchTree{
         public Node<E> parent; 
         public E value;
     
-        public Node(Node<E> _left, Node<E> _right, Node<E> _parent, E _value){
-    
-            this.left = _left;
-            this.right = _right;
-            this.parent = _parent;
-            this.value = _value;
-            
-        }
-    
         public Node(E _value){
             this.parent = null;
-            this.left = null;
-            this.right = null;
-            this.value = _value;
-        }
-
-        public Node(Node<E> _parent, E _value){
-            this.parent = _parent;
             this.left = null;
             this.right = null;
             this.value = _value;
