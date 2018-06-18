3b212226860ac59d42f4b80cdb778d4ac1c54ee8
unknown <zi.s.gao@gmail.com>
2018-06-18 06:48:31 -0400

    Started priorityQueue.java

---
+++ Data Structures/Java/PriorityQueue.java
@@ -0,0 +1,37 @@
+/* Priority Queue Implementation using Heaps (Binary tree such that parent is always >= children) 
+
+Insertion is random, we do not care, but extract Max always returns the greatest node
+
+*/
+
+public class PriorityQueue {
+    /* Remember to make complete trees using pointer to keep track of latest leaf */
+
+    public PriorityQueue(){
+    }
+
+    public int extractMax(){
+        return 0;
+    }
+    
+    public int getMax(){
+        return 0;
+    }
+
+    public void remove(){
+    }
+
+    public void insert(){
+    }
+
+    private void siftup(){
+    }
+
+    private void siftdown(){
+    }
+
+    public String toString(){
+        return "";
+    }
+
+}
