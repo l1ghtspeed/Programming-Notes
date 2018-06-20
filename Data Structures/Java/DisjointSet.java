ecf775eb4c380885c4239f61989017b4b3cff4b1
unknown <zi.s.gao@gmail.com>
2018-06-20 06:21:43 -0400

    Completed disjoint set implementation

--- Data Structures/Java/DisjointSet.java
+++ Data Structures/Java/DisjointSet.java
@@ -2,40 +2,56 @@
 
 Disjoint set data structure using forest of tree data structures - makes it easy to keep id of head
 
+Tree data structure implemented using array to save time and space complexity
+
 One way to solve if you can get from point A to point B in a Maze.
 
 */
 
 
 public class DisjointSet{
+    public int[] rank;
+    public int[] forest;
 
-    private Node<E> root = new Node<E>();
-
-    public DisjointSet(){  
-        
+    public DisjointSet(int size){
+        this.rank = new int[size];
+        this.forest = new int[size];      
     }
 
-    public void union(){
+    public void Union(int t1, int t2){
+        int t1Parent = this.Find(t1);
+        int t2Parent = this.Find(t2);
+        if (t1Parent != t2Parent){
+            if (this.rank[t1Parent] > this.rank[t2Parent]){
+                this.forest[t2Parent] = t1Parent;
+            } else {
+                this.forest[t1Parent] = t2Parent;
+                if (this.rank[t1Parent] == this.rank[t2Parent]){
+                    this.rank[t2Parent] += 1;
+                }
+            }
+        } 
     }
 
     public void MakeSet(int value){
+        this.forest[value] = value;
+        this.rank[value] = 1;
     }
 
-    public void Find(){
+    public int Find(int value){
+        int id = value;
+        while(this.forest[id] != id){
+            id = this.forest[value];
+        }
+        return id;
     } 
 
-    private class Node<E> {
-        public Node<E> parent; 
-        public E value;
-    
-        public Node(E _value){
-            this.parent = null;
-            this.value = _value;
+    public String toString(){
+        String temp = "";
+        for (int i = 0; i < this.forest.length; i++){
+            temp += "(" + i + ", " + this.forest[i] + ") ";
         }
-
-        public boolean isLeaf(){
-            return this.left != null && this.right != null;
-        }
+        return temp;
     }
 
 }
