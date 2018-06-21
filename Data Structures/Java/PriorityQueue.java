f097556066e0c5f2f87c54d6dfdf4ce187f3bf30
Zi <zi.s.gao@gmail.com>
2018-06-21 16:21:34 -0400

    changed priority que implemenation to use array as a tree

--- Data Structures/Java/PriorityQueue.java
+++ Data Structures/Java/PriorityQueue.java
@@ -6,25 +6,36 @@ Insertion is random, we do not care, but extract Max always returns the greatest
 
 public class PriorityQueue {
     /* Remember to make complete trees using pointer to keep track of latest leaf */
-    private Node<E> root = new Node<E>();
+    private int[] arr;
     
-
     public PriorityQueue(){
-
+        this.arr = new int[7];
     }
 
     public int extractMax(){
-        return 0;
+        int temp = arr[0];
+        this.remove(arr[0]);
+        return temp;
     }
     
     public int getMax(){
-        return 0;
+        return arr[0];
     }
 
     public void remove(){
     }
 
-    public void insert(){
+    private int getRight(int id){
+        return this.arr[id*2+1];
+    }
+
+    private int getLeft(int id){
+        return this.arr[id*2];
+    }
+
+
+
+    public void insert(E value){
     }
 
     private void siftup(){
@@ -34,25 +45,32 @@ public class PriorityQueue {
     }
 
     public String toString(){
-        return "";
+        String temp = "";
+        Node<E> curr = this.root;
+        temp = this.inOrderTraversal(curr);
+        return temp;
     }
 
-    private class Node<E> {
-        public Node<E> left;
-        public Node<E> right; 
-        public Node<E> parent; 
-        public E value;
-    
-        public Node(E _value){
-            this.parent = null;
-            this.left = null;
-            this.right = null;
-            this.value = _value;
-        }
+    private String inOrderTraversal(Node<E> curr){
+        String temp = "";
+        if (curr.left != null){
+            this.inOrderTraversal(curr.left);
+        } 
 
-        public boolean isLeaf(){
-            return this.left != null && this.right != null;
-        }
+        temp += curr.value;
+        temp += " ";
+
+        if (curr.right != null){
+            this.inOrderTraversal(curr.right);
+        } 
+        return temp;
     }
 
+    private void enlarge(){
+        int[] temp = this.arr;
+        this.arr = new int[temp.length*2+1];
+        for (int i = 0; i < temp.length; i++){
+            this.arr[i] = temp[i];
+        }
+    }
 }
