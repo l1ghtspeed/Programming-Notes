8d1bd86c46fe22c909cf68edfad7cebd346aa89a
unknown <zi.s.gao@gmail.com>
2018-06-23 19:19:16 -0400

    Created medium practice problems hackerrank

---
+++ Practice Problems/HackerRank Problems/MediumPracticeProblems.py
@@ -0,0 +1,12 @@
+# File containing all the 'medium' level problems solved on HackerRank
+# Function name is name of problem
+
+def matchingStrings(strings, queries):
+    arr = []
+    for i in queries:
+        k = 0
+        for j in strings:
+            if i == j:
+                k+=1
+        arr.append(k)
+    return arr
