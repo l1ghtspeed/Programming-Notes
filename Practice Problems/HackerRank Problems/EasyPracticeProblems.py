294ea161af07b1ca4f17c2a5736e18bb2df55b59
unknown <zi.s.gao@gmail.com>
2018-06-23 19:17:24 -0400

    Added easy problems for hackerrank

---
+++ Practice Problems/HackerRank Problems/EasyPracticeProblems.py
@@ -0,0 +1,51 @@
+# A Collection of all the 'easy' problems on Hacker Rank
+# Function Name is name of problem solved
+
+def simpleArraySum(ar):
+    count = 0
+    for i in ar:
+        count+=i 
+    return count
+
+def CompleteTheTriplets(a, b):
+    ap = 0
+    bp = 0
+    for i in range(3):
+        if (a[i] < b[i]):
+            bp += 1
+        elif (a[i] > b[i]):
+            ap += 1
+    return str(ap) + str(bp)
+
+
+def pickingNumbers(a):
+    count = 0
+    
+    for i in a:
+        uppercount = 0
+        lowercount = 0
+        for j in a:
+            if j == i:
+                uppercount+=1
+                lowercount+=1
+            elif j == i+1:
+                uppercount+=1
+            elif j == i-1:
+                lowercount+=1
+        if uppercount >= count:
+            count = uppercount
+        if lowercount >= count:
+            count = lowercount
+    return count
+
+def countApplesAndOranges(s, t, a, b, apples, oranges):
+    numApples = 0
+    numOranges = 0
+    for i in apples:
+        if (a + i >= s and a + i <= t):
+            numApples+=1
+    for i in oranges:
+        if (b + i >= s and b + i <= t):
+            numOranges+=1
+    print(numApples)
+    print(numOranges)
