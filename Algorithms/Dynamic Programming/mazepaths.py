e1c9b1664d64409dd1e42d1d28b7715b28bf18e0
unknown <zi.s.gao@gmail.com>
2018-07-29 15:21:40 -0400

    moved folder, started new DP practice problem

---
+++ Algorithms/Dynamic Programming/mazepaths.py
@@ -0,0 +1,6 @@
+# Problem: given a NxN grid, and certain obstacles, give the total number of possible paths to solve that maze.
+
+# First implement naive recursive solution
+
+def solveMaze():
+    return 1
