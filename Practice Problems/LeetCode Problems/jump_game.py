bc294ee176a81ea02b24954094329b8b6a5a830a
Zi <Zi.s.gao@gmail.com>
2020-01-08 19:21:48 -0500

    Create jump_game.py

---
+++ Practice Problems/LeetCode Problems/jump_game.py
@@ -0,0 +1,26 @@
+# n^2 solution, currently TLE
+
+from collections import deque
+
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        if not nums:
+            return False
+        
+        seen = set([0])
+        q = deque([0])
+        
+        while q:
+            elem = q.popleft()
+            if elem == len(nums) - 1:
+                return True
+            for i in range(1, nums[elem]+1):
+                pos_a, pos_b = i + elem, elem - i
+                if pos_a < len(nums) and pos_a not in seen:
+                    seen.add(pos_a)
+                    q.append(pos_a)
+                if pos_b >= 0 and pos_b not in seen:
+                    seen.add(pos_b)
+                    q.append(pos_b)
+                
+        return False
